958c5b4f3620488aa746f1e499f16cbe7bbca106
Walker Flynn <walker.d.flynn@gmail.com>
2013-04-28 16:17:07 -0700

    recursion working using map

--- recursion/src/problem.js
+++ recursion/src/problem.js
@@ -2,4 +2,11 @@
 // [1,2,33,[99,22,[88,[100],22,334],9000],3,9001,[315,68,88,[290],11],57]
 
 var recursiveNumPrinter = function(deeplyNestedArray) {
-};
+	return jQuery.map(deeplyNestedArray, function(item){
+		if(item.length){
+		  return recursiveNumPrinter(item);
+		} else {
+		  return '' + item;
+		}
+	}).join("-");
+}
